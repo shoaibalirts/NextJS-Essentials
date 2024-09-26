e0e8c08d3d8b8e2a5ca2e74ae263f0cb5429f3f1
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-26 15:39:13 +0200

    folder name [slug] is a placeholder where we can add multiple posts agains each blog. using params.slug can be useful when reaching out to the database and to fetch posts from the database using params

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
+import Header from "@/components/header";
 export default function Home() {
   console.log("Executing home page..");
 
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to NextJS Framework built on React Library!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         <Link href="./about">About</Link>
